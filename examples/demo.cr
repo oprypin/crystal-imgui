9d5c0b2402f12d136e44a621b4d4e6971245ff3e
Oleh Prypin <oleh@pryp.in>
2020-09-19 17:39:17 +0200

    Add font loading to example

--- examples/demo.cr
+++ examples/demo.cr
@@ -8,6 +8,14 @@ window = SF::RenderWindow.new(SF::VideoMode.new(1280, 720), title)
 window.framerate_limit = 60
 ImGui::SFML.init(window)
 
+io = ImGui.get_io
+if File.file?(font_path = "/usr/share/fonts/droid/DroidSansJapanese.ttf")
+  font_config = ImGui::ImFontConfig.new.tap { |c| c.merge_mode = true}
+  io.fonts.add_font_from_file_ttf(font_path, 18, font_config, io.fonts.get_glyph_ranges_japanese)
+  io.fonts.build
+  ImGui::SFML.update_font_texture
+end
+
 delta_clock = SF::Clock.new
 while window.open?
   while (event = window.poll_event)
